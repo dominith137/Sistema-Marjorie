9fa004a7284dffee7e342f8302c96bf2efcd7959
Gabriel Esteban Gutierez Quispe <gabriel.gutierrezq@vallegrande.edu.pe>
2025-10-27 22:40:12 -0500

    main actualizado

--- src/main/java/org/usil/Main.java
+++ src/main/java/org/usil/Main.java
@@ -5,14 +5,19 @@ import org.usil.Controlador.ServicioControlador;
 import org.usil.Vista.ClienteVista;
 import org.usil.Vista.ServicioVista;
 
-import javax.swing.*;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ServicioControlador c = new ServicioControlador();
-            ServicioVista v = new ServicioVista(c);
-            v.mostrar();
+            // Servicios
+            /* ServicioControlador servicioCtrl = new ServicioControlador();
+            ServicioVista servicioUI = new ServicioVista(servicioCtrl);
+            servicioUI.mostrar(); */
+
+            ClienteControlador clienteCtrl = new ClienteControlador();
+            ClienteVista clienteUI = new ClienteVista(clienteCtrl);
+            clienteUI.mostrar();
         });
     }
 }
