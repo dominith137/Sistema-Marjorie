f07940eaf4dad64d97c04c2af2132b849f1ecae8
Gabriel Esteban Gutierez Quispe <gabriel.gutierrezq@vallegrande.edu.pe>
2025-11-21 22:50:44 -0500

    Se remplza los enum por una interface y agrega clases para aplicar para state

---
+++ src/main/java/org/usil/Modelo/EstadoCitaCancelada.java
@@ -0,0 +1,27 @@
+package org.usil.Modelo;
+
+public class EstadoCitaCancelada implements EstadoCita {
+
+    @Override
+    public String getNombreEspanol() {
+        return "Cancelada";
+    }
+
+    @Override
+    public boolean permiteModificacion() {
+        return false;
+    }
+
+    @Override
+    public boolean permiteCancelacion() {
+        return false;
+    }
+
+    @Override
+    public void completar(Cita cita) {
+    }
+
+    @Override
+    public void cancelar(Cita cita) {
+    }
+}
