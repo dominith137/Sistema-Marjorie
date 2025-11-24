b896b6eeeca9fb75b8dd43cd09996c5b96dfc62c
Gabriel Esteban Gutierez Quispe <gabriel.gutierrezq@vallegrande.edu.pe>
2025-11-23 20:40:13 -0500

    se modifico reportefactory para que devuelva reporteformato

--- src/main/java/org/usil/Controlador/ReporteFactory.java
+++ src/main/java/org/usil/Controlador/ReporteFactory.java
@@ -1,13 +1,22 @@
 package org.usil.Controlador;
 
-import org.usil.Modelo.Reporte;
+import org.usil.Modelo.ReporteFormato;
 import org.usil.Modelo.ReportePDF;
+import org.usil.Modelo.ReporteConsola;
 
 public class ReporteFactory {
-    public static Reporte crearReporte(String tipo) {
+
+    public static ReporteFormato crearFormato(String tipo) {
+        if (tipo == null) {
+            throw new IllegalArgumentException("Tipo de formato no puede ser nulo");
+        }
+
         if (tipo.equalsIgnoreCase("pdf")) {
             return new ReportePDF();
+        } else if (tipo.equalsIgnoreCase("consola")) {
+            return new ReporteConsola();
         }
-        throw new IllegalArgumentException("Tipo de reporte no soportado: " + tipo);
+
+        throw new IllegalArgumentException("Tipo de formato no soportado: " + tipo);
     }
 }
